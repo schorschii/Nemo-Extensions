20a32294175e79bdc7bf8a9a11a885324a41f1f1
Georg Sieber <it@georg-sieber.de>
2021-03-03 13:02:53 +0100

    fix davinci audio action

--- action-convert-davinci/davinci-audio.sh
+++ action-convert-davinci/davinci-audio.sh
@@ -4,9 +4,7 @@
 
 for var in "$@"; do
 
-	# urldecoding: replace %20 with space
-	fileUri=$var
-	filePath=${fileUri//%20/ }
+	filePath="file://$var"
 
 	ffmpeg -i "$filePath" -acodec pcm_s16le -vcodec copy "$filePath.converted.mov" \
 		| zenity --progress --text "Please wait...\n$filePath" --auto-close --pulsate --auto-kill --no-cancel
