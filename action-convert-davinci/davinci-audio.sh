c7e77737ac58b01e314c9c9c3c18133667818e09
Georg Sieber <it@georg-sieber.de>
2023-07-31 23:19:20 +0200

    add overwrite option, closes #3

--- action-convert-davinci/davinci-audio.sh
+++ action-convert-davinci/davinci-audio.sh
@@ -2,11 +2,21 @@
 
 # author: schorschii
 
-for var in "$@"; do
+overwrite=NO
+if zenity --question --text="Do you want to overwrite the original file(s)?"; then
+	overwrite=YES
+fi
 
-	filePath="file://$var"
+for file in "$@"; do
+
+	filePath="file://$file"
 
 	ffmpeg -i "$filePath" -acodec pcm_s16le -vcodec copy "$filePath.converted.mov" \
 		| zenity --progress --text "Please wait...\n$filePath" --auto-close --pulsate --auto-kill --no-cancel
+
+	if [ "$overwrite" == "YES" ]; then
+		rm "$file"
+		mv "$file.converted.mov" "$file"
+	fi
 
 done
